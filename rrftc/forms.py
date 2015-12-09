67653aadda406e123cbc57018fc40dc58d8f42dd
rahm <j.rahm@f5.com>
2015-12-09 16:31:55 -0600

    sorting update

--- rrftc/forms.py
+++ rrftc/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, PasswordField, IntegerField, validators, DateField, HiddenField, TextAreaField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField, validators, DateField, HiddenField, TextAreaField, SelectField, BooleanField
 from wtforms_sqlalchemy.fields import QuerySelectField
 
-from models import Team
+from models import Team, Scout, Competition
 '''
 from models import Team, Scout, Competition
 
@@ -15,6 +15,51 @@ def active_scouts():
 def active_teams():
     return Team.query.all()
 '''
+
+class ScoutingForm(Form):
+    competition = QuerySelectField(query_factory=lambda: Competition.query.all(), get_label='Name')
+    team = QuerySelectField(query_factory=lambda: Team.query.all(), get_label='Number')
+    scout = QuerySelectField(query_factory=lambda: Scout.query.all(), get_label='Name')
+    rweight = IntegerField('Robot Weight', [validators.DataRequired('What is the robot\'s Weight?')], default=0)
+    rheight = IntegerField('Robot Height', [validators.DataRequired('What is the robot\'s Height?')], default=0)
+    auto = BooleanField('Autonomous?', default=False)
+    beacon = BooleanField('Push Beacon?', default=False)
+    aclimbers = BooleanField('Deliver Climbers?', default=False)
+    lclimber = BooleanField('Rescue Low Climber?', default=False)
+    mclimber = BooleanField('Rescue Mid Climber?', default=False)
+    hclimber = BooleanField('Rescue High Climber?', default=False)
+    fpark = BooleanField('Park on Floor?', default=False)
+    lpark = BooleanField('Park on Low Zone?', default=False)
+    mpark = BooleanField('Park on Mid Zone?', default=False)
+    hpark = BooleanField('Park on High Zone?', default=False)
+    climbheight = SelectField('Highest Zone Climbed', choices=[('Floor', 'Floor'),
+                                                               ('Low Zone', 'Low Zone'),
+                                                               ('Mid Zone', 'Mid Zone'),
+                                                               ('High Zone', 'High Zone')])
+    debris = BooleanField('Can Score Debris?', default=False)
+    ldebrisscore = BooleanField('Can Score Debris in Low Zone?', default=False)
+    mdebrisscore = BooleanField('Can Score Debris in Mid Zone?', default=False)
+    hdebrisscore = BooleanField('Can Score Debris in High Zone?', default=False)
+    avgdebris = IntegerField('Average Debris Score?', [validators.DataRequired('How much debris is scored on average?')], default=0)
+    debrisscoringmethod = SelectField('How is Debris Handled?', choices=[('Climb', 'Climb'),
+                                                                         ('Launch', 'Launch'),
+                                                                         ('Extend', 'Extend'),
+                                                                         ('Other', 'Other')])
+    hang = BooleanField('Can the robot hang?', default=False)
+    allclear = BooleanField('Trigger All Clear?', default=False)
+    pof = TextAreaField('Points of Failure', [validators.DataRequired'Please enter possible points of failure:'], default='points of failure:')
+    comments = TextAreaField('Comments', [validators.DataRequired('Please enter comments:')], default='general comments:')
+
+    submit = SubmitField('Add Report')
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        else:
+            return True
 
 class CompetitionTeamForm(Form):
     competition = HiddenField('Competition')
