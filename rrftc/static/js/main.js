39cd6d2c239779c794e3d96582a905d44bec6365
rahm <j.rahm@f5.com>
2016-02-15 18:48:37 -0600

    added jquery option to hide/show fields based on autonomous selection.

--- rrftc/static/js/main.js
+++ rrftc/static/js/main.js
@@ -1,3 +1,13 @@
 /**
  * Created by rahm on 2/14/16.
  */
+
+jQuery(document).ready(function() {
+    $("input#auto.form-control").change(function () {
+        if (this.checked) {
+            $('div#beacon.form-group').show();
+        } else {
+            $('div#beacon.form-group').hide();
+        }
+    });
+});
